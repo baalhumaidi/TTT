935cc162342f5dc1bcc2867de087d2476fdeb23f
baalhumaidi <93419731+baalhumaidi@users.noreply.github.com>
2021-12-06 19:27:16 +0000

    TTTgame

--- TTT/ViewController.swift
+++ TTT/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var gameButtons: [UIButton]!
+ 
+    var turn : Int = 1
+
+ 
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+//        buttons.append(button0)
+//        buttons.append(button1)
+//        buttons.append(button2)
+//        buttons.append(button3)
+//        buttons.append(button4)
+//        buttons.append(button5)
+//        buttons.append(button6)
+//        buttons.append(button7)
+//        buttons.append(button8)
+        for button in gameButtons {
+            print (button.tag)
+        }
+        turn = 1
+        print (turn)
+       
+   
+    }
+ 
+    @IBAction func changeColor(_ sender: UIButton) {
+        if turn == 1
+        {
+            sender.backgroundColor = UIColor.red
+            
+            turn = 2
+        }else{
+            sender.backgroundColor = UIColor.blue
+            turn = 1
+        }
+        sender.isEnabled = false
+        print (sender.tag)
+        print (turn)
+
+     
+    
+
+        
     }
 
 
+   
+
+    
 }
 
